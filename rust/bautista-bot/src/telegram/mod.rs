cfb213b25f94ca5384f604f19ad77a8dcb380984
Iván Zaera Avellón <ivan.zaera@protonmail.com>
2022-10-24 14:44:18 +0200

    style: clean code

--- rust/bautista-bot/src/telegram/mod.rs
+++ rust/bautista-bot/src/telegram/mod.rs
@@ -12,17 +12,6 @@ mod api;
 pub enum TelegramError {
     #[error("Telegram API call failed")]
     CallFailed,
-    /*
-    #[error("data store disconnected")]
-    Disconnect(#[from] io::Error),
-    #[error("the data for key `{0}` is not available")]
-    Redaction(String),
-    #[error("invalid header (expected {expected:?}, found {found:?})")]
-    InvalidHeader {
-        expected: String,
-        found: String,
-    },
-    */
 }
 
 pub struct Bot<'a> {
