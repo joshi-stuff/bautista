b68a3bcd8300a5871a713a7f38f04b0440c6d1c5
Iván Zaera Avellón <ivan.zaera@protonmail.com>
2022-10-24 17:23:28 +0200

    feat: finish implementation of rule cheap

--- rust/bautista-bot/src/rule/cheap.rs
+++ rust/bautista-bot/src/rule/cheap.rs
@@ -36,20 +36,39 @@ impl RuleEval for RuleCheap {
         } else {
             let mut prices = prices.today().unwrap();
 
-            if self.consecutive {
-                // TODO: Implement cheap consecutive rule
-                self.on_at = None
-            } else {
-                let mut on_at: Vec<bool> = vec![false; 24];
+            let mut on_at: Vec<bool> = vec![false; 24];
 
+            if self.consecutive {
+                let mut cheapest_hour: i64 = 0;
+                let mut cheapest_sum = i64::MAX;
+
+                for hour in 0..24 - self.hours {
+                    let mut sum = 0;
+
+                    for i in 0..self.hours {
+                        sum += prices[(hour + i) as usize];
+                    }
+
+                    eprintln!("> {} {}", &hour, &sum);
+
+                    if sum < cheapest_sum {
+                        cheapest_hour = hour;
+                        cheapest_sum = sum;
+                    }
+                }
+
+                for i in 0..self.hours {
+                    on_at[(cheapest_hour + i) as usize] = true;
+                }
+            } else {
                 prices.sort();
 
                 for i in 0..self.hours {
                     on_at[prices[i as usize] as usize] = true;
                 }
-
-                self.on_at = Some(on_at);
             }
+
+            self.on_at = Some(on_at);
         }
     }
 }
